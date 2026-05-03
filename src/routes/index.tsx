4463d961b5eb69bbb61a0dd2fe0999d5d186ba73
Obinna Anosike <smartlify09@gmail.com>
2026-05-03 12:55:01 +0100

    ch: update responsiveness

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,21 @@
 import { createFileRoute, Link } from "@tanstack/react-router"
 import { buttonVariants } from "@/components/ui/button"
 import { ContestantsList } from "@/features/contestants/components/contestants-list"
+import { cn } from "@/lib/utils"
 
 export const Route = createFileRoute("/")({ component: App })
 
 function App() {
   return (
-    <div className="flex min-h-svh items-center p-12">
+    <div className="flex min-h-svh items-center p-6 lg:p-12">
       <section className="flex w-full flex-col gap-6">
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-1 lg:flex-row lg:items-start lg:items-center justify-between">
 
-          <h1 className="text-center text-3xl font-medium">
+          <h1 className="lg:text-center text-3xl font-medium">
             Vote for your spec!
           </h1>
 
-          <Link to="/register" className={buttonVariants({ variant: "default", })}>Register as a contestant</Link>
+          <Link to="/register" className={cn(buttonVariants({ variant: "default", }), "self-start lg:self-stretch")}>Register as a contestant</Link>
         </div>
         <ContestantsList />
       </section>
