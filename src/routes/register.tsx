fe7da9f8d9dc83f160410b64c0129f383860bba9
Obinna Anosike <smartlify09@gmail.com>
2026-05-05 23:32:58 +0100

    ch:fix padding

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -44,7 +44,7 @@ function RegisterPage() {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
-      <div className="flex items-center min-h-screen justify-center p-4 md:p-12 pt-24 md:pt-36">
+      <div className="flex items-center min-h-screen justify-center p-4 md:p-12 pt-36 md:pt-36">
         <div className="w-full lg:max-w-2xl">
           <div className="absolute top-24 right-4 md:right-8 lg:right-12 flex items-center gap-2 text-sm md:text-base font-medium text-muted-foreground">
             <span>Voting ends in:</span>
